43d9aeeda7cdf8270fd849db3de259ddecf96625
Mofassarul <m.hoque3478@gmail.com>
2021-08-31 13:16:01 -0400

    math problem solve 2

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -1,6 +1,7 @@
 package math.problems;
 
 import javax.swing.*;
+import java.util.Scanner;
 
 public class PrimeNumber {
 
@@ -12,6 +13,34 @@ public class PrimeNumber {
 
          Print out the prime numbers in the given range.
          */
+
+        Scanner scanner = new Scanner(System.in);
+        int i =0;
+        int num =0;
+
+        String  primeNumbers = "";
+        System.out.println("Enter the value of n:");
+        int n = scanner.nextInt();
+        scanner.close();
+        for (i = 1; i <= n; i++)
+        {
+            int counter=0;
+            for(num =i; num>=1; num--)
+            {
+                if(i%num==0)
+                {
+                    counter = counter + 1;
+                }
+            }
+            if (counter ==2)
+            {
+                //Appended the Prime number to the String
+                primeNumbers = primeNumbers + i + " ";
+            }
+        }
+        System.out.println("Prime numbers from 1 to n are :");
+        System.out.println(primeNumbers);
+
 
     }
 
