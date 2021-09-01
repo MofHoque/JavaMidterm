8f21804176a5fe0fb28d7e834b5a6f2eb7fc8fae
Mofassarul <m.hoque3478@gmail.com>
2021-09-01 16:40:48 -0400

    String problem solved

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -9,5 +9,22 @@ public class Permutation {
 
         Write Java program to compute all permutations of any given String
          */
+        String letters = "ABC";
+         permutation(letters);
     }
+    public static void permutation(String letters) {
+        permutation("", letters);
+    }
+    private static void permutation(String pre, String letters){
+        int n = letters.length();
+        if(n==0){
+            System.out.println(pre);
+        }
+        else {
+            for(int i = 0; i<n; i++){
+                permutation(pre+ letters.charAt(i), letters.substring(0,i) + letters.substring(i+1, n));
+            }
+        }
+    }
+
 }
